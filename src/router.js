a86453268beb3f6b64975c81f12d068db206b512
xumaozeng <15611895341@163.com>
2020-12-03 18:27:02 +0800

    [ADD] 用户面板

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage/IndexPage';
+import React from "react";
+import { Router, Route, Switch } from "dva/router";
+import IndexPage from "./routes/IndexPage/IndexPage";
+import User from "./routes/User";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/users" component={User} />
       </Switch>
     </Router>
   );
---
+++ src/routes/User/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <h3>User</h3>
+      </div>
+    );
+  }
+}
+export default User;
