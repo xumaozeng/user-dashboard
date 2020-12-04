231e2ddba39e8a79d11676ad4151f1c3fc7ca916
xumaozeng <15611895341@163.com>
2020-12-04 09:09:13 +0800

    [ADD] antd组件

--- src/routes/User/index.js
+++ src/routes/User/index.js
@@ -1,12 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "dva";
+import { Table, Button } from "antd";
+import styles from "./index.css";
 
-class User extends Component {
-  render() {
-    return (
-      <div>
-        <h3>User</h3>
+function User({ dispatch, list: dataSource, loading, total, page: current }) {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a href="">{text}</a>
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email"
+    },
+    {
+      title: "Website",
+      dataIndex: "website",
+      key: "website"
+    },
+    {
+      title: "Operation",
+      key: "operation",
+      render: (text, record) => <span>删除</span>
+    }
+  ];
+  return (
+    <div className={styles.normal}>
+      <div className={styles.create}>
+        <Button type="primary">Create User</Button>
       </div>
-    );
-  }
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        loading={loading}
+        rowKey={(record) => record.id}
+        pagination={false}
+      />
+    </div>
+  );
 }
-export default User;
+
+function mapStateToProps(state) {
+  const { list, total, page } = state.user;
+  return {
+    loading: state.loading.models.user,
+    list,
+    total,
+    page
+  };
+}
+
+export default connect(mapStateToProps)(User);
